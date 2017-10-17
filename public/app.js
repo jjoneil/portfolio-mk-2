3e820aa086afe8c3c461dce219e5bcce9ece4856
John O'Neil <johnoneil@Rons-MacBook-Air-2.local>
2017-10-17 15:33:32 -0600

    first

---
+++ public/app.js
@@ -0,0 +1,25 @@
+$('document').ready(function(){
+  $('nav').hide().fadeIn(1000);
+  $('#app').hide().fadeIn(2500);
+
+
+  $('body').scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.box').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1500);
+                    
+            }
+            
+        }); 
+
+    });
+});
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1>My Portfolio<h1>
+
+<p>this one is going to be a single page, with vue components, switching out the content of a rectangular box taking up the center of a non-scrollable, single page<p>
+
+<p>top nav will control the switching of components in the vue</p>
